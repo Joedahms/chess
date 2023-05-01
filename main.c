6367a49cae8214384bfafad993e5ea401cc668e2
Joedahms <dahms.joejoe@gmail.com>
2023-04-30 21:27:45 -0400

    playing with bitmaps

--- main.c
+++ main.c
@@ -2,15 +2,45 @@
 // each tile can have a pointer a piece struct
 // matrix of piece pointers 
 
+// FEN code:
+// rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1
+// 1. piece positions. lowercase is black, uppercase is white
+// 2. active color
+// 3. castling rights
+// 4. possible en passant targets
+// 5. half move clock. reset upon capture or pawn move
+// 6. full move clock. starts at one. increases when a player moves
+//
+// Bitboards. 64 bit binary number. each bit is a position on the board.
+// lsb is bottom left. msb is top right. increases from left to right, top 
+// to bottom
+//
+// all white pieces
+// all black pieces
+// black pawns
+// white pawns
+// black rooks
+// white rooks
+// black knights
+// white knights
+// black bishops
+// white bishops
+// black queen
+// white queen
+// black king
+// white king
+
 #include <curses.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <aio.h>
 #include <signal.h>
+#include <math.h>
 
 #include "setup.h"
 #include "utility.h"
 #include "tile.h"
+#include "draw_board.h"
 
 #include "pawn.h"
 
@@ -19,13 +49,30 @@ struct aiocb cntrl_blk;
 
 struct tile board[8][8]; // array of tiles
 
+int num = 0b101;
+int num2 = 0b110;
+int bin_num[64] = {1,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,1};
+
+
 void shutdown();
 void setup_cntrl_blk();
 void on_input(int);
 void init_tiles();
+void int_to_bin(int);
+void int_to_bin64(unsigned long);
+void draw_bitmap(int, int, int *);
 
 int main(void) {
 	setup();
+	unsigned long input = (unsigned long)pow(2,63);
+	//int_to_bin64(input);
 
 
 	init_tiles();
@@ -34,6 +81,18 @@ int main(void) {
 	signal(SIGIO, on_input); 
 	aio_read(&cntrl_blk);
 
+/*
+	bin_num = {1,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0}
+		   */
+	draw_bitmap(0, 150, &bin_num[0]);
+
 	while (running) {
 		pause();	
 		refresh();
@@ -41,7 +100,6 @@ int main(void) {
 
 
 	shutdown();
-
 	return 0;
 }
 
@@ -114,4 +172,63 @@ void init_tiles() {
 	}
 
 	
+}
+void draw_bitmap(int y_pos, int x_pos, int* bitmap) {
+	int i;
+	int j;
+	int init_x_pos = x_pos;
+	draw_board(y_pos, x_pos, 1, 8);
+	y_pos += 15;
+	x_pos++;
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			mvprintw(y_pos, x_pos, "%d", *bitmap);
+			x_pos += 3;
+			bitmap++;
+		}
+		y_pos -= 2;
+		x_pos = init_x_pos + 1;
+	}
+}
+
+void int_to_bin64(unsigned long integer) {
+	int i;
+	int index = 0; // start point in array
+	int power = 63;
+	unsigned long rem; // remainder
+
+	bin_num[index] = integer / (unsigned long)pow(2, power); // msb
+	index++; // index = 1
+	
+	rem = integer % (unsigned long)pow(2, power); 
+	power--;
+
+	while (power >= 0) { // 2^n, n >= 0
+		bin_num[index] = rem / (unsigned long)pow(2, power); // bit 
+		rem = rem % (unsigned long)pow(2, power); // remainder
+		power--;
+		index++;
+	}
+
+}
+
+// take a decimal number and convert it to binary
+void int_to_bin(int integer) {
+	int i;
+	int index = 0; // start point in array
+	int power = floor(log2f(integer)); // power of bit under msb
+	int rem; // remainder
+
+	bin_num[index] = integer / (int)pow(2, power); // msb
+	index++; // index = 1
+	
+	rem = integer % (int)pow(2, power); 
+	power--;
+
+	while (power >= 0) { // 2^n, n >= 0
+		bin_num[index] = rem / pow(2, power); // bit 
+		rem = rem % (int)pow(2, power); // remainder
+		power--;
+		index++;
+	}
 }
--- makefile
+++ makefile
@@ -1,22 +1,22 @@
 all: chess
 
 chess: main.o draw_board.o setup.o utility.o pawn.o
-	gcc main.o draw_board.o setup.o utility.o pawn.o -o chess -lncurses -lrt
+	gcc main.o draw_board.o setup.o utility.o pawn.o -o chess -lncurses -lm
 
-main.o: main.c setup.h utility.h pawn.h tile.h
-	gcc -c main.c -lncurses -lrt
+main.o: main.c setup.h utility.h pawn.h tile.h draw_board.h
+	gcc -c main.c
 
 draw_board.o: draw_board.c draw_board.h
-	gcc -c draw_board.c -lncurses
+	gcc -c draw_board.c
 
 setup.o: setup.c setup.h draw_board.h
-	gcc -c setup.c -lncurses
+	gcc -c setup.c
 
 utility.o: utility.c utility.h
-	gcc -c utility.c -lncurses
+	gcc -c utility.c
 
 pawn.o: pawn.c pawn.h
-	gcc -c pawn.c -lncurses
+	gcc -c pawn.c
 
 clean: 
 	rm chess
